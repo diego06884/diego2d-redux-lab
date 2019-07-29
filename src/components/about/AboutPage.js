985f0f285c15832354fe765a1954eb0abb629e64
Diego Vergara <diego06884@hotmail.com>
2019-07-29 00:59:08 -0500

    Initial Setup
    Add initial components
    Add Router configuration
    Add PageNotFound component

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This page uses React, Redux and other React related technologies.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
